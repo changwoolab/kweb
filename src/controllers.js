46a2f00361071f3fc8580f9caca4558a5553f10c
CWY <cwyoo01@naver.com>
2021-12-23 17:56:23 +0900

    exam

--- src/controllers.js
+++ src/controllers.js
@@ -8,11 +8,11 @@ router.get('/express/:isExpress(\\d+)', async (req, res, next) => {
         const express = req.params.isExpress;
         if (express == 1) {
             const sql = `select name from stations where isExpress = 1`;
-            const results = await runQuery(sql, [name]);
+            const results = await runQuery(sql, []);
             return results;
         } else if (express == 0) {
             const sql = `select name from stations where isExpress = 0`;
-            const results = await runQuery(sql, [name]);
+            const results = await runQuery(sql, []);
             return results;
         } else {
             throw new Error('BAD_REQUEST');
@@ -26,7 +26,7 @@ router.get('/:passengers(\\d+)', async (req, res, next) => {
     try {
         const pass = req.params.passengers;
         const sql = `select name from stations where passengers > ${pass}`;
-        const results = await runQuery(sql, [name]);
+        const results = await runQuery(sql, []);
         return results;
     } catch(err) {
         return next(err);
